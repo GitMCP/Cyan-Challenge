0aa7d71e4e635199ee0e7680211de000be513045
Gustavo <gustavomaurocamacho@hotmail.com>
2020-09-05 19:36:31 -0300

    Changed DB credentials for production

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,15 @@
 module.exports = {
   dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'cyan',
-  database: 'cyan',
+  host: 'ec2-34-238-26-109.compute-1.amazonaws.com',
+  username: 'lsssanlwrdweem',
+  password: '28e410a0dcbb067e10968459a38690fa3f064197869230a6bf50155d7ee7cf8f',
+  database: 'db92momrmchjov',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
   define: {
     timestamps: true,
     underscored: true,
